99901b53401c42cd60e1a34963617e593553640c
Eric Ho <renovaeric@gmail.com>
2022-04-28 10:46:19 +0800

    1st commit

---
+++ app/Main.js
@@ -0,0 +1,7 @@
+import React, { useState, useReducer } from "react"
+import ReactDOM from "react-dom"
+
+function Main() {
+  return <h3>hihi</h3>
+}
+ReactDOM.render(<Main />, document.querySelector("#app"))
